9a18a48d2ac27f8b167e9ab83b0977b4d4317efd
S-P-H-E <sphe.g.personal@gmail.com>
2025-08-04 11:51:03 +0200

    loading state

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+"use client"
 import Brands from "@/components/Brands";
 import addToWaitlist from "@/lib/actions";
+import clsx from "clsx";
+import { useState } from "react";
+import { PulseLoader } from "react-spinners";
 
 export default function Home() {
+  const [loading, setLoading] = useState(false)
+
   return (
     <div className="flex flex-col justify-center items-center h-[100dvh]">
       {/* Form */}
@@ -10,9 +16,15 @@ export default function Home() {
         <h1 className="text-6xl font-semibold text-center"><span className="font-extralight">GET EARLY</span> ACCESS</h1>
         <p className="text-[var(--description)] px-6 md:p-0 md:w-lg text-center">Be the first to try our AI-powered job application filter that automatically screens candidates and finds your perfect hires!</p>
 
-        <form action={addToWaitlist} className="flex gap-4 border px-5 py-3 rounded-2xl border-[var(--border)] bg-[var(--gray-background)]">
+        <form action={addToWaitlist} onSubmit={() => setLoading(true)} className="flex gap-4 border px-5 py-3 rounded-2xl border-[var(--border)] bg-[var(--gray-background)]">
           <input type="email" name="email" className="outline-none md:w-xs" placeholder="your@email.com" required autoComplete="on" autoFocus />
-          <button type="submit" className="cursor-pointer hover:scale-105 transition-all bg-[var(--foreground)] text-[var(--background)] px-4 py-2 rounded-xl font-semibold shadow-lg">Join waitlist</button>
+          <button type="submit" disabled={loading} className={clsx(loading ? "flex gap-2 items-center justify-center w-[130px] h-[50px]" :  "w-[130px] h-[50px]", "bg-[var(--foreground)] text-[var(--background)] rounded-xl font-semibold shadow-lg cursor-pointer  hover:scale-105 transition-all")}>
+            {loading ? (
+              <>
+                <PulseLoader color='white' size={5}/>
+              </>  
+            ): "Join waitlist"}
+          </button>
         </form>
       </div>
 
