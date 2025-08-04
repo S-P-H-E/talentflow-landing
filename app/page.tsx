36547afbccd916219ea6edea4236c19946cfd505
S-P-H-E <sphe.g.personal@gmail.com>
2025-08-04 15:14:44 +0200

    autocomplete

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <p className="text-[var(--description)] px-6 md:p-0 md:w-lg text-center">Be the first to try our AI-powered job application filter that automatically screens candidates and finds your perfect hires!</p>
 
         <form action={addToWaitlist} onSubmit={() => setLoading(true)} className="flex gap-4 border px-5 py-3 rounded-2xl border-[var(--border)] bg-[var(--gray-background)]">
-          <input type="email" name="email" className="outline-none md:w-xs" placeholder="your@email.com" required autoComplete="on" autoFocus />
+          <input type="email" name="email" className="outline-none md:w-xs" placeholder="your@email.com" required autoFocus/>
           <button type="submit" disabled={loading} className={clsx(loading ? "flex gap-2 items-center justify-center w-[130px] h-[50px]" :  "w-[130px] h-[50px]", "bg-[var(--foreground)] text-[var(--background)] rounded-xl font-semibold shadow-lg cursor-pointer  hover:scale-105 transition-all")}>
             {loading ? (
               <>
