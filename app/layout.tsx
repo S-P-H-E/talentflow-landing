3b30bdb9c240d1ed670c959cf3b66b3a501fb55a
S-P-H-E <sphe.g.personal@gmail.com>
2025-08-04 14:07:59 +0200

    analytics position change

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
       <body
         className={`${inter.variable} antialiased`}
       >
-        <Analytics />
         {children}
+        <Analytics />
       </body>
     </html>
   );
