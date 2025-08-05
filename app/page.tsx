6a63a9ddd92173306d8077f3ee9babb67c928b43
S-P-H-E <sphe.g.personal@gmail.com>
2025-08-05 14:51:10 +0200

    input fix FUHHHH, I SHOULD HAVE DONE THIS EARLIER

--- app/page.tsx
+++ app/page.tsx
@@ -32,9 +32,9 @@ export default function Home() {
         <p className="text-[var(--description)] px-6 md:p-0 md:w-lg text-center motion-delay-300 motion-opacity-in-0 motion-translate-y-in-150">Be the first to try our AI-powered job application filter that automatically screens candidates and finds your perfect hires!</p>
 
         <div className="motion-delay-500 motion-opacity-in-0 motion-translate-y-in-150">
-          <form action={addToWaitlist} onSubmit={() => setLoading(true)} className="flex gap-4 border px-5 py-3 rounded-2xl border-[var(--border)] bg-[var(--gray-background)]">
-            <input type="email" name="email" className="outline-none md:w-xs" placeholder="your@email.com" required autoFocus/>
-            <button type="submit" disabled={loading} className={clsx(loading ? "flex gap-2 items-center justify-center w-[130px] h-[50px]" :  "w-[130px] h-[50px]", "bg-[var(--foreground)] text-[var(--background)] rounded-xl font-semibold shadow-lg cursor-pointer  hover:scale-105 transition-all")}>
+          <form action={addToWaitlist} onSubmit={() => setLoading(true)} className="flex gap-4 items-center">
+            <input type="email" name="email" className="outline-none md:w-xs border px-5 py-4 rounded-2xl border-[var(--border)] bg-[var(--gray-background)]" placeholder="your@email.com" required autoFocus/>
+            <button type="submit" disabled={loading} className={clsx(loading ? "flex gap-2 items-center justify-center w-[130px] h-[50px]" :  "w-[130px] py-4", "bg-[var(--foreground)] text-[var(--background)] rounded-xl font-semibold shadow-lg cursor-pointer  hover:scale-105 transition-all")}>
               {loading ? (
                 <>
                   <PulseLoader color='white' size={5}/>
